26e36349f3e1c7d60be8a4144e80e6602a22a69d
tack13 <sub.fxxk@gmail.com>
2020-07-27 11:58:12 +0900

    レイアウト作成
    レイアウト関連のコンポネントをcomponentsフォルダに移行

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import styled, { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import logo from './logo.svg'
+import logo from '../logo.svg'
 import './App.css'
-import { Path } from './routes'
+import { Path } from '../routes'
 
 const App: React.FC = () => {
   return (
     <>
-      <GlobalStyle />
       <Wrapper>
         <Header>
           <Logo src={logo} className='App-logo' alt='logo' />
@@ -25,21 +24,6 @@ const App: React.FC = () => {
   )
 }
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif
-    -webkit-font-smoothing: antialiased
-    -moz-osx-font-smoothing: grayscale
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace
-  }
-`
 const Wrapper = styled.div`
   text-align: center
 `
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
-import App from './App'
-import { Otameshi } from './Otameshi'
+import App from './components/App'
+import { Layout } from './components/Layout'
+import { Otameshi } from './components/Otameshi'
 
 export const Path = {
 	app: '/',
@@ -9,11 +10,13 @@ export const Path = {
 }
 
 const routes = (
-	<Switch>
-		<Route exact path={Path.app} component={App} />
-		<Route exact path={Path.otameshi} component={Otameshi} />
-		<Redirect to={Path.app} />
-	</Switch>
+	<Layout>
+		<Switch>
+			<Route exact path={Path.app} component={App} />
+			<Route exact path={Path.otameshi} component={Otameshi} />
+			<Redirect to={Path.app} />
+		</Switch>
+	</Layout>
 )
 
 export default routes
