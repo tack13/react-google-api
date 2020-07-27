b593ce17a2dd98c0c5d3e115886e1f41350975c2
tack13 <sub.fxxk@gmail.com>
2020-07-27 17:05:32 +0900

    API Clientの作成

--- src/components/GoogleBooks/index.tsx
+++ src/components/GoogleBooks/index.tsx
@@ -1,33 +1,9 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { State } from '../../reducers'
 import { GoogleBooksActions } from '../../actions/googleBooks'
-import { VolumeList } from '../../models/Volume'
 import { SearchResult } from './SearchResult'
-
-const searchGoogleBooks = async (searchString: string) => {
-  const url = 'https://www.googleapis.com/books/v1/volumes'
-  const params = {
-    q: searchString
-  }
-  try {
-    const response = await axios.get(url, { params })
-    return {
-      isSuccess: true,
-      data: response.data,
-      error: null
-    }
-  } catch (error) {
-    return {
-      isSuccess: false,
-      data: null,
-      error
-    }
-  }
-}
-
 
 export const GoogleBooks: React.FC = () => {
   const [searchString, changeSearchString] = useState('')
