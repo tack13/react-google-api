2559b850e8b8d9ccbec6f14b27026c77648ba825
tack13 <sub.fxxk@gmail.com>
2020-07-27 15:03:42 +0900

    Reduxを導入する
    command
    - $ npm add redux react-redux redux-logger typescript-fsa typescript-fsa-reducers connected-react-router
    - $ npm add -D @types/react-redux @types/redux-logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createBrowserHistory } from 'history'
-import { Router } from 'react-router-dom'
 import routes from './routes'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import { configureStore, history } from './configureStore'
 import * as serviceWorker from './serviceWorker'
 
-const history = createBrowserHistory()
+const store = configureStore()
 
 ReactDOM.render(
-  <Router history={history}>
-    {routes}
-  </Router>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>{routes}</ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 )
 
---
+++ src/reducers/index.ts
@@ -0,0 +1,13 @@
+import { History } from 'history'
+import { combineReducers } from 'redux'
+import { RouterState, connectRouter } from 'connected-react-router'
+
+export interface State {
+  router: RouterState
+}
+
+export const rootReducer = (history: History) => combineReducers(
+  {
+    router: connectRouter(history)
+  }
+)
